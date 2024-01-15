f38a64944bd1c48525a93746244664d5672a7c79
jeff <zx8989123@gmail.com>
2024-01-15 14:43:52 +0800

    updata

---
+++ WebApplication3/Controllers/helloworld.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class helloworld : ControllerBase
+    {
+        public helloworld() { }
+
+        [HttpGet]
+        public string hellowold()
+        {
+            return "Hellow world";
+        }
+    }
+}
